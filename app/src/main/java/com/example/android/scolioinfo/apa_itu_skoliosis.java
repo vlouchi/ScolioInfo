4b797d4455153495bbe03b04309ceca4f393a52b
I. F. Thufailah <fairuzthufailah@gmail.com>
2017-05-10 19:42:14 +0700

    uploading layout

---
+++ app/src/main/java/com/example/android/scolioinfo/apa_itu_skoliosis.java
@@ -0,0 +1,13 @@
+package com.example.android.scolioinfo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class apa_itu_skoliosis extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apa_itu_skoliosis);
+    }
+}
---
+++ app/src/main/java/com/example/android/scolioinfo/gejala_penyebab.java
@@ -0,0 +1,13 @@
+package com.example.android.scolioinfo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class gejala_penyebab extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gejala_penyebab);
+    }
+}
